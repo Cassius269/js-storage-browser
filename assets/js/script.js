296c76c57d6c1594d82680a70a5ba5588bb3187a
Cassius269 <fahamygaston@gmail.com>
2025-07-23 00:21:21 +0200

    lecture de cookie spécifique et suppression

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,15 @@ document.cookie = 'darkmode=true; max-age=86400';
 console.log(document.cookie);
 
 // Lire spécifiquement le contenu d'un cookie
+function getCookie(name) {
+  let cookie = {};
+  document.cookie.split(';').forEach(function(el) {
+    let split = el.split('=');
+    cookie[split[0].trim()] = split.slice(1).join("=");
+  })
+  return cookie[name];
+}
+
+document.cookie="username=; expires=Mon";
+// console.log(getCookie('darkmode'))
+console.log(document.cookie)
