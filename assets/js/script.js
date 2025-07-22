4a92864a4c653ca4e24fe23960d2f4961dfb8081
Cassius269 <fahamygaston@gmail.com>
2025-07-23 00:53:23 +0200

    création, lecture et suppression de données dans la sessionStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 console.log('hello world');
 
+///----------PARTIE 1. LES COOKIES --------///
 // Ecrire le nom d'utilisateur dans un cookie
 document.cookie = `username=john-dow@exemple.com; max-age=86400`;
 document.cookie = 'darkmode=true; max-age=86400';
@@ -19,4 +20,16 @@ function getCookie(name) {
 
 document.cookie="username=; expires=Mon";
 // console.log(getCookie('darkmode'))
-console.log(document.cookie)
+console.log(document.cookie);
+
+///----------PARTIE 1. La SessionStorage --------///
+// Stockage en session de l'email et nom de l'utilisateur
+sessionStorage.setItem('email', 'john-dow@exemple.com');
+sessionStorage.setItem('name', 'JOHN Dow');
+
+// Lecture des données en sessionStorage
+let email = sessionStorage.getItem('email');
+console.log('email', email);
+
+// Suppression de l'email de l'utilisateur en session
+sessionStorage.removeItem('email');
